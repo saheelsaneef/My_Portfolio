bd5decbd269529a2aeca5c8eb7b1dc891eaccd30
saheelsaneef <saneef1231@gmail.com>
2024-01-05 02:01:40 +0530

    add icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,48 +5,69 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLocation } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 const Contact = () => {
     return ( 
         <div>
             <h3 className="text-center mt-5">Contact me</h3>
+
             <Container>
+
             <div className='d-flex '>
+
                 <div className="details">
                     <h3 className='fw-bold'>Get in Touch</h3>
                     <p>Hello!!! Let's Talk</p>
+
                     <div className="Name d-flex">
+
                         <div className="icon1 ">
-                            <p>Hello everyone</p>
+                        <FontAwesomeIcon icon={faUser} />
                         </div>
+
                         <div className="name">
-                            <p className='mb-0 fw-bold'>Name</p>
+                            <p className='mb-0 fw-bold '>Name</p>
                             <p>Saheel Saneef</p>
                         </div>
+
                     </div>
+
                     <div className="Address d-flex">
+
                         <div className="icon2 ">
-                            <p>Hello everyone</p>
+                        <FontAwesomeIcon icon={faLocation} />
                         </div>
+
                         <div className="name">
                             <p className='mb-0 fw-bold'>Address</p>
                             <p>333/19,Sakkariya lane, Kuttikarachi,Kinniya -04</p>
                         </div>
+
                     </div>
+
                     <div className="Email d-flex">
+
                         <div className="icon3 ">
                         <FontAwesomeIcon icon={faEnvelope} />
                         </div>
+
                         <div className="name">
                             <p className='mb-0 fw-bold'>Email</p>
                             <p>saneef1231@gmail.com</p>
                         </div>
+
                     </div>
 
                 </div>
+
                 <div className="form ">
                     <h3 className='fw-bold '>Message Me</h3>
+
                     <Form>
+
                         <Row className="mb-3">
                             <Form.Group as={Col} controlId="formGridname">
                                 <Form.Control type="name" placeholder="Name" />
@@ -68,9 +89,13 @@ const Contact = () => {
                         <Button variant="danger" type="submit">Send Message</Button>
 
                     </Form>
+
                 </div>
+
             </div>
+
             </Container>
+            
         </div>
         
      );
