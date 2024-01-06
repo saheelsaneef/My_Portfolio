2904629b819f8d5f57c15db425eee3632bd3a493
saheelsaneef <saneef1231@gmail.com>
2024-01-07 04:05:17 +0530

    adjust all image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { useSpring, animated } from 'react-spring';
+import styled from 'styled-components';
 import planetOne from '../Assets/planet.svg';
 import planetTwo from '../Assets/planet-2.svg';
 import planetThree from '../Assets/planet-3.svg';
@@ -23,25 +24,37 @@ const Home = () => {
     config: { duration: 1200 },
   });
 
+  const Img = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%; 
+  max-width:220px;
+  max-height:220px;
+`;
+
+
   return (
-    <div className='bg-black vh-100 d-flex align-items-center'>
+    
+    <div className='bg-black vh-100 d-flex  align-items-center'>
         
+        <div className="left">
             <Container>
-                <animated.h1 className='fs-2 text-white mb-3' style={{ ...fadeInAnimation }}>
-                    Hello my name is
-                </animated.h1>
-                <animated.h1 className="fw-bold text-white mb-3" style={{ fontSize: '75px', ...fadeInAnimat }}>
-                    {name}
-                </animated.h1>
-                <animated.p className='text-white' style={{ fontSize: '35px', ...fadeInAnimation }}>
-                    And I'm an <span className='text-danger fw-bold' >Undergraduate</span>
-                </animated.p>
-                <img src={planetOne} alt='planet1'></img>
-                <img src={planetTwo} alt='planet1'></img>
-                <img src={planetThree} alt='planet1'></img>
-                <img src={planetFour} alt='planet1'></img>
-            </Container>     
+                <animated.h1 className='fs-2 text-white mb-3' style={{ ...fadeInAnimation }}>Hello my name is</animated.h1>
+                <animated.h1 className="fw-bold text-white mb-3" style={{ fontSize: '75px', ...fadeInAnimat }}> {name}</animated.h1>
+                <animated.p className='text-white' style={{ fontSize: '35px', ...fadeInAnimation }}>And I'm an <span className='text-danger fw-bold' >Undergraduate</span></animated.p>
+            </Container>
+        </div>
+        <div className="right ">
+            <Container>
+                <Img src={planetOne} alt='planet1'/>
+                <Img src={planetTwo} alt='planet2'/>
+                <Img src={planetThree} alt='planet3'/>
+                <Img src={planetFour} alt='planet4'/>
+            </Container>
+        </div>
+        
     </div>
+
   );
 };
 
