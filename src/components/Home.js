6f778d1660dba13d4e5d781b3a7d01c87885e8cd
saheelsaneef <saneef1231@gmail.com>
2024-01-07 03:08:52 +0530

    add image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,48 @@
-import Container from 'react-bootstrap/Container';
-// import styled from 'styled-components';
-// import { motion } from 'framer-motion';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { useSpring, animated } from 'react-spring';
+import planetOne from '../Assets/planet.svg';
+import planetTwo from '../Assets/planet-2.svg';
+import planetThree from '../Assets/planet-3.svg';
+import planetFour from '../Assets/planet-4.svg';
+
 
 const Home = () => {
+  const name = "Saheel Saneef";
 
-    const name = "Saheel Saneef";
+  const fadeInAnimation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 1500 },
+  });
 
-    
-    return ( 
-        <div className='bg-black vh-100 d-flex align-items-center '>
-            <Container >
-                <h1 className='fs-2 text-white mb-3'>Hello my name is</h1>
-                <h1 className="fw-bold text-white mb-3" style={{ fontSize: '75px' }}>{name}</h1>
-                <p className='text-white'style={{ fontSize: '35px' }}>And I'm an <span className='text-danger fw-bold'>Undergraduate</span> </p>
-            </Container>
-        </div>
+  const fadeInAnimat = useSpring({
+    opacity: 1,
+    transform: 'translateX(0%)',
+    from: { opacity: 0, transform: 'translateX(-100%)' },
+    config: { duration: 1200 },
+  });
+
+  return (
+    <div className='bg-black vh-100 d-flex align-items-center'>
         
-        
-        
-     );
-}
- 
+            <Container>
+                <animated.h1 className='fs-2 text-white mb-3' style={{ ...fadeInAnimation }}>
+                    Hello my name is
+                </animated.h1>
+                <animated.h1 className="fw-bold text-white mb-3" style={{ fontSize: '75px', ...fadeInAnimat }}>
+                    {name}
+                </animated.h1>
+                <animated.p className='text-white' style={{ fontSize: '35px', ...fadeInAnimation }}>
+                    And I'm an <span className='text-danger fw-bold' >Undergraduate</span>
+                </animated.p>
+                <img src={planetOne} alt='planet1'></img>
+                <img src={planetTwo} alt='planet1'></img>
+                <img src={planetThree} alt='planet1'></img>
+                <img src={planetFour} alt='planet1'></img>
+            </Container>     
+    </div>
+  );
+};
+
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import { Navbar,Nav,Container } from 'react-bootstrap';
 
 const NavBar = () => {
  
-
   return ( 
     <Navbar expand="lg" className= "bg-black" sticky='top' >
       <Container>
@@ -11,7 +10,7 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end ">
           <Nav className="mr-auto ">
-            <Nav.Link href="#home" className="text-white fs-5 fw-600 ">Home</Nav.Link>
+            <Nav.Link href="#home" className="text-white fs-5 fw-600 " >Home</Nav.Link>
             <Nav.Link href="#about" className="text-white fs-5 fw-600 ">About</Nav.Link>
             <Nav.Link href="#service" className="text-white fs-5 fw-600">Projects</Nav.Link>
             <Nav.Link href="#skills" className="text-white fs-5 fw-600">Skills</Nav.Link>
